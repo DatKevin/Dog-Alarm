4d75ac5a8ecfceedd411f77a57ca7dc8135f1cb2
Kevin Dat Nguyen <datkevin@gmail.com>
2020-07-03 15:39:57 -0700

    initial commit

---
+++ reminder-bot.py
@@ -0,0 +1,53 @@
+import logging
+import schedule
+import time
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
+def start(update, context):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi!')
+
+
+def help_command(update, context):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
+
+
+def main():
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater("953051145:AAElkgFrnYZ01tElx-7zpvDjMHHOkaQPmvQ", use_context=True)
+
+    # Get the dispatcher to register handlers
+    dispatch = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dispatch.add_handler(CommandHandler("start", start))
+    dispatch.add_handler(CommandHandler("help", help_command))
+
+    # on noncommand i.e message - echo the message on Telegram
+    #dispatch.add_handler(MessageHandler(Filters.text, echo))
+
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
